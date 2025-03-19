e31dfbc12fd0327f1fe1b546a790000b392c0f95
Ho Thanh Nha <hothanhnha25062004@gmail.com>
2025-03-19 09:24:45 +0700

    docker compose

--- SPRINGBOOT/identity-service/src/main/java/com/devteria/identity/entity/User.java
+++ SPRINGBOOT/identity-service/src/main/java/com/devteria/identity/entity/User.java
@@ -14,7 +14,7 @@ import lombok.experimental.FieldDefaults;
 @NoArgsConstructor
 @AllArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE)
-@Entity
+@Entity(name = "Users")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
