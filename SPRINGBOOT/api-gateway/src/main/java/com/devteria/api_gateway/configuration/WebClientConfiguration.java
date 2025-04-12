3bb41f145a9a17339a5c26fb851325add7b44ca5
Ho Thanh Nha <hothanhnha25062004@gmail.com>
2025-04-12 16:12:44 +0700

    update cors

--- SPRINGBOOT/api-gateway/src/main/java/com/devteria/api_gateway/configuration/WebClientConfiguration.java
+++ SPRINGBOOT/api-gateway/src/main/java/com/devteria/api_gateway/configuration/WebClientConfiguration.java
@@ -1,12 +1,17 @@
 package com.devteria.api_gateway.configuration;
 
 import com.devteria.api_gateway.repository.IdentityClient;
-import com.devteria.api_gateway.service.IdentityService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.reactive.function.client.support.WebClientAdapter;
 import org.springframework.web.service.invoker.HttpServiceProxyFactory;
+
+import java.util.List;
+
 
 @Configuration
 public class WebClientConfiguration {
@@ -18,10 +23,26 @@ public class WebClientConfiguration {
     }
 
     @Bean
+    CorsWebFilter corsWebFilter(){
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.setAllowedOrigins(List.of("*"));
+        corsConfiguration.setAllowedHeaders(List.of("*"));
+        corsConfiguration.setAllowedMethods(List.of("*"));
+
+        UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
+        urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
+
+        corsConfiguration.setAllowCredentials(true);
+
+        return new CorsWebFilter(urlBasedCorsConfigurationSource);
+    }
+
+    @Bean
     IdentityClient identityClient(WebClient webClient){
         HttpServiceProxyFactory httpServiceProxyFactory = HttpServiceProxyFactory
                 .builderFor(WebClientAdapter.create(webClient)).build();
 
         return httpServiceProxyFactory.createClient(IdentityClient.class);
     }
+
 }
